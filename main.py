e07246fbe4c33027bd42f19009e6a2f49049e48d
unknown <mail@dstrelnikov.com>
2021-02-15 12:27:53 +0000

    Добавил поддержку списка словарей

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import mymodule
 import os
 print(os.getcwd())
-mymodule.hello()
-print(mymodule.fib(10))
-f = open('Figurlist.txt')
-print(f.read())
+#mymodule.hello()
+
+with open('Figurlist.txt', 'r') as f:
+    fig = f.read.splitlines()
+
+
+while (f):
+    line = f.readline
+    
+
+f = open()
+try:
+    assadsad
+finally:
+    f.close()
+
+assadsad
+
+for x in fig: print(mymodule.fib(x))
--- mymodule.py
+++ mymodule.py
@@ -1,8 +1,23 @@
+# class Triangle(object):
+     
+#     def __init__ (self, x1,x2,y1,y2,z1,z2):
+#         self.x1=x1
+#         self.x2=x2
+#         self.y1=y1
+#         self.y2=y2
+#         self.z1=z1
+#         self.z2=z2
+
+#      def printview(self):
+#          return "x"
+
+#     def square(self):  
+#         return "y"
+
+
 def hello():
     print('Hello, world!')
 
 def fib(n):
-    a = b = 1
-    for i in range(n - 2):
-        a, b = b, a + b
+    b='d'+n
     return b
